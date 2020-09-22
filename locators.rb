f9378c50c4c53c9ea6ae50396f0a143ae57f22f0
maksimok93 <maksimok.dnepr@gmail.com>
2020-09-22 11:56:41 +0300

    Initialized UITestingWithRuby local repository.

---
+++ locators.rb
@@ -0,0 +1,34 @@
+module EntryPageLocators
+  LOGIN = 'input[type="submit"]'
+end
+
+module HeaderLocators
+  OPENSOURCECMS = 'li[id="wp-admin-bar-site-name"]'
+  UPDATE = 'li[id="wp-admin-bar-updates"]'
+  EDIT_COMMENTS = 'li[id="wp-admin-bar-comments"]'
+  NEW = 'li[id="wp-admin-bar-new-content"]'
+end
+
+module PostsLocators
+  PUBLISH = 'button[class="components-button editor-post-publish-panel__toggle is-button is-primary"]'
+  CONFIRM_PUBLISH = 'button[class="components-button editor-post-publish-button is-button is-default is-primary is-large"]'
+end
+
+module DashboardLocators
+  SCREEN_OPTIONS = 'button[id="show-settings-link"]'
+end
+
+module UsersLocators
+  USERS = 'li[id="menu-users"]'
+  ADD_NEW_USER = 'input[id="createusersub"]'
+  SHOW_PASSWORD = 'button[class="button wp-generate-pw hide-if-no-js"]'
+  APPLY = 'input[id="doaction2"]'
+  ACTIONS = 'bulk-action-selector-bottom'
+end
+
+module Links
+  ENTRY_PAGE = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/index.php'
+  NEW_POST = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/post-new.php'
+  NEW_USER = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/user-new.php'
+  USERS = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/users.php'
+end
---
+++ pages.rb
@@ -0,0 +1,103 @@
+require './locators'
+require 'capybara/dsl'
+
+def get_file_content(filename)
+  IO.readlines("#{filename}")
+end
+
+class BasePage
+  include Capybara::DSL
+  def open_main_page
+    visit('/')
+    fill_in 'user_login', :with => get_file_content("credentials")[0]
+    fill_in 'user_pass', :with => get_file_content("credentials")[0]
+    find(EntryPageLocators::LOGIN).click
+  end
+
+  def click_opensourcecms
+    find(HeaderLocators::OPENSOURCECMS).click
+  end
+
+  def click_update_button
+    find(HeaderLocators::UPDATE).click
+  end
+
+  def click_edit_comments
+    find(HeaderLocators::EDIT_COMMENTS).click
+  end
+
+  def click_users
+    find(UsersLocators::USERS).click
+  end
+
+  def new_dropdown
+    select('Post', from: HeaderLocators::NEW)
+  end
+end
+
+module Pages
+  include Capybara::DSL
+
+  class Dashboard < BasePage
+    def update_screen_options(option, mode="uncheck")
+      find(DashboardLocators::SCREEN_OPTIONS).click
+      case mode
+      when "uncheck"
+        uncheck(option)
+      else
+        check(option)
+      end
+      find(DashboardLocators::SCREEN_OPTIONS).click
+    end
+
+    def click_update_button
+      click_button('Update Now')
+    end
+  end
+
+  class Posts < BasePage
+    def add_new_post(user_input)
+      visit(Links::NEW_POST)
+      fill_in 'Add title', :with => user_input
+      find(PostsLocators::PUBLISH).click
+      find(PostsLocators::CONFIRM_PUBLISH).click
+    end
+  end
+
+  class Users < BasePage
+    def visit_users_page
+      visit(Links::USERS)
+    end
+
+    def add_new_user(username, email, first_name, last_name, website, password)
+      visit(Links::NEW_USER)
+      fill_in 'Username', :with => username
+      fill_in 'Email', :with => email
+      fill_in 'First Name', :with => first_name
+      fill_in 'Last Name', :with => last_name
+      fill_in 'Website', :with => website
+      find(UsersLocators::SHOW_PASSWORD).click
+      fill_in 'Password', :with => password
+      find(UsersLocators::ADD_NEW_USER).click
+    end
+
+    def choose_user(name)
+      click_link("#{name}")
+    end
+
+    def edit_user(role, first_name, last_name, website=nil)
+      select "#{role}", from: "role"
+      fill_in 'First Name', :with => first_name
+      fill_in 'Last Name', :with => last_name
+      fill_in 'Website', :with => website
+      click_button('Update User')
+    end
+
+    def delete_user(name)
+      check("Select #{name}")
+      select 'Delete', from: UsersLocators::ACTIONS
+      find(UsersLocators::APPLY).click
+      click_button('Confirm Deletion')
+    end
+  end
+end
