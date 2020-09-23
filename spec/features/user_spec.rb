1d2298ac410e9634f65d8485ee9ce5bad43cf6fe
maksimok93 <maksimok.dnepr@gmail.com>
2020-09-23 15:43:24 +0300

    Added before(:each) method to user_spec.rb with setup option before running each test.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -8,31 +8,25 @@ require_relative '../../locators'
 feature 'OpenSourceCMS test', type: :feature do
 
   describe Pages::Users do
-    it "#add_new_user" do
-      session = Pages::Users.new
-      session.open_main_page
-      session.visit_users_page
-      session.add_new_user('John', 'test@example.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
+    before(:each) do
+      @session = Pages::Users.new
+      @session.open_main_page
+      @session.visit_users_page
+    end
 
+    it "#add_new_user" do
+      @session.add_new_user('John', 'test@example.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
       expect(page).to have_content("New user created.")
     end
 
     it "#edit_user" do
-      session = Pages::Users.new
-      session.open_main_page
-      session.visit_users_page
-      session.choose_user('John')
-      session.edit_user('Author', 'Mark', 'Mich')
-
+      @session.choose_user('John')
+      @session.edit_user('Author', 'Mark', 'Mich')
       expect(page).to have_content("User updated.")
     end
 
     it "#delete_user" do
-      session = Pages::Users.new
-      session.open_main_page
-      session.visit_users_page
-      session.delete_user('John')
-
+      @session.delete_user('John')
       expect(page).to have_content("User deleted.")
     end
   end
