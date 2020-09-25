e4f89712472b6e89a1255319851782743ed91661
maksimok93 <maksimok.dnepr@gmail.com>
2020-09-25 16:08:27 +0300

    Tests were divided into stories: When on New User page, When on User page.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,38 +2,19 @@ require_relative '../spec_helper'
 
 feature 'OpenSourceCMS test', type: :feature do
 
-  describe 'When on Users Page' do
+  describe 'When on New User Page' do
     before(:each) do
       @users_page = Users.new
       @users_page.open_main_page
-      @users_page.visit_users_page
+      @users_page.visit_new_user_page
     end
 
     it 'Verifies user can be created' do
-      @users_page.visit_new_user_page
       @users_page.add_new_user('John', 'test@example.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
       expect(page).to have_content('New user created.')
     end
 
-    it 'Verifies user can be edited' do
-      @users_page.choose_user('John')
-      @users_page.edit_user('Author', 'Mark', 'Mich')
-      expect(page).to have_content('User updated.')
-    end
-
-    it 'Verifies user can be deleted' do
-      @users_page.delete_user('John')
-      expect(page).to have_content('User deleted.')
-    end
-  end
-
-  describe 'Negative user stories when on Users Page' do
-    before(:each) do
-      @users_page = Users.new
-      @users_page.open_main_page
-      @users_page.visit_users_page
-    end
-    it "'Verifies user can't be created if username already exists'" do
+    it "Verifies user can't be created if username already exists" do
       @users_page.add_new_user('Michael', 'test@test.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
       expect(page).to have_content('This username is already registered. Please choose another one.')
     end
@@ -46,6 +27,25 @@ feature 'OpenSourceCMS test', type: :feature do
     it "Verifies user can't be created if e-mail isn't correct" do
       @users_page.add_new_user('Nick', 'test', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
       expect(page).to have_content('The email address isn’t correct.')
+    end
+  end
+
+  describe 'When on User Page' do
+    before(:each) do
+      @users_page = Users.new
+      @users_page.open_main_page
+      @users_page.visit_users_page
+    end
+
+    it 'Verifies user can be edited' do
+      @users_page.choose_user('John')
+      @users_page.edit_user('Author', 'Mark', 'Mich')
+      expect(page).to have_content('User updated.')
+    end
+
+    it 'Verifies user can be deleted' do
+      @users_page.delete_user('John')
+      expect(page).to have_content('User deleted.')
     end
   end
 end
