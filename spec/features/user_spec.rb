85c37b4cf349af0a351bcd279d408bf92885560f
maksimok93 <maksimok.dnepr@gmail.com>
2020-09-30 10:09:59 +0300

    In user_spec.rb test data (first_name, last_name, website, password) has been moved to before(:each) block.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,25 +7,30 @@ feature 'OpenSourceCMS test', type: :feature do
       @users_page = Users.new
       @users_page.open_main_page
       @users_page.visit_new_user_page
+
+      @first_name = 'John'
+      @last_name = 'Smith'
+      @website = 'google.com'
+      @password = 'Password<>12348989898989'
     end
 
     it 'Verifies user can be created' do
-      @users_page.add_new_user('John', 'test@example.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
+      @users_page.add_new_user('John', 'test@example.com', @first_name, @last_name, @website, @password)
       expect(page).to have_content('New user created.')
     end
 
     it "Verifies user can't be created if username already exists" do
-      @users_page.add_new_user('Michael', 'test@test.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
+      @users_page.add_new_user('Michael', 'test@test.com', @first_name, @last_name, @website, @password)
       expect(page).to have_content('This username is already registered. Please choose another one.')
     end
 
     it "Verifies user can't be created if e-mail already exists" do
-      @users_page.add_new_user('Nick', 'test@ex.com', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
+      @users_page.add_new_user('Nick', 'test@ex.com', @first_name, @last_name, @website, @password)
       expect(page).to have_content('This email is already registered, please choose another one.')
     end
 
     it "Verifies user can't be created if e-mail isn't correct" do
-      @users_page.add_new_user('Nick', 'test', 'John', 'Smith', 'google.com', 'Password<>12348989898989')
+      @users_page.add_new_user('Nick', 'test', @first_name, @last_name, @website, @password)
       expect(page).to have_content('The email address isn’t correct.')
     end
 
