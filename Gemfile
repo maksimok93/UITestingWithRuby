800a927b48c8bf8d21bfccf86aaa663016cde05c
maksimok93 <maksimok.dnepr@gmail.com>
2020-10-01 11:42:58 +0300

    Added messages_data; users_data text files. Added let() method to user_spec.rb

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@
 
 source "https://rubygems.org"
 
-gem "rspec"
-gem "capybara"
-gem "selenium-webdriver"
-gem "rake"
+gem 'rspec'
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'rake'
+gem 'rubocop', require: false
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,43 +1,53 @@
 require_relative '../spec_helper'
 
 feature 'OpenSourceCMS test', type: :feature do
+  describe 'When on New User Page,' do
 
-  describe 'When on New User Page' do
-    #TODO Let paramemetrized
-    # TODO Hash
-    before(:each) do
-      @users_page = Users.new
-      @users_page.open_main_page
-      @users_page.visit_new_user_page
+    let(:user) { eval(File.open('users_data') { |file| file.read } ) }
+    let(:messages) { eval(File.open('messages_data') { |file| file.read } ) }
 
-      @first_name = 'John'
-      @last_name = 'Smith'
-      @website = 'google.com'
-      @password = 'Password<>12348989898989'
+    describe 'when all test data is valid' do
+      before(:each) do
+        @users_page = Users.new
+        @users_page.open_main_page
+        @users_page.visit_new_user_page
+        @users_page.add_new_user(
+            user[:username],
+            user[:email],
+            user[:first_name],
+            user[:last_name],
+            user[:website],
+            user[:password])
+      end
+
+      it 'Verifies user can be created' do
+        expect(page).to have_content(messages[:success_register])
+      end
+
+      it "Verifies user can't be created if username already exists" do
+        expect(page).to have_content(messages[:warning_username])
+      end
+
+      it "Verifies user can't be created if e-mail already exists" do
+        expect(page).to have_content(messages[:warning_email])
+      end
+
+      it 'Verifies if Add New User button is enabled' do
+        expect(page).to have_button('Add New User')
+      end
     end
 
-    it 'Verifies user can be created' do
-      @users_page.add_new_user('John', 'test@example.com', @first_name, @last_name, @website, @password)
-      expect(page).to have_content('New user created.')
-    end
+    describe 'when specific test data is invalid' do
+      before(:each) do
+        @users_page = Users.new
+        @users_page.open_main_page
+        @users_page.visit_new_user_page
+      end
 
-    it "Verifies user can't be created if username already exists" do
-      @users_page.add_new_user('Michael', 'test@test.com', @first_name, @last_name, @website, @password)
-      expect(page).to have_content('This username is already registered. Please choose another one.')
-    end
-
-    it "Verifies user can't be created if e-mail already exists" do
-      @users_page.add_new_user('Nick', 'test@ex.com', @first_name, @last_name, @website, @password)
-      expect(page).to have_content('This email is already registered, please choose another one.')
-    end
-
-    it "Verifies user can't be created if e-mail isn't correct" do
-      @users_page.add_new_user('Nick', 'test', @first_name, @last_name, @website, @password)
-      expect(page).to have_content('The email address isn’t correct.')
-    end
-
-    it 'Verifies if Add New User button is enabled' do
-      expect(page).to have_button('Add New User')
+      it "Verifies user can't be created if e-mail isn't correct" do
+        @users_page.add_new_user(user[:username], 'test', user[:first_name], user[:last_name], user[:website], user[:password])
+        expect(page).to have_content(messages[:incorrect_email])
+      end
     end
   end
 
@@ -54,14 +64,14 @@ feature 'OpenSourceCMS test', type: :feature do
       expect(page).to have_content('User updated.')
     end
 
+    it 'Verifies users role can be edited' do
+      @users_page.change_users_role('John', 'Editor')
+      expect(page).to have_content('Changed roles.')
+    end
+
     it 'Verifies user can be deleted' do
       @users_page.delete_user('John')
       expect(page).to have_content('User deleted.')
-    end
-
-    it 'Verifies users role can be edited' do
-      @users_page.change_users_role('Kyle', 'Editor')
-      expect(page).to have_content('Changed roles.')
     end
 
     it 'Verifies if Search Users button is enabled' do
