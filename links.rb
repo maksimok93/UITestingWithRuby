ee4735e243a43e4e912bffe62eadd346943587ab
maksimok93 <maksimok.dnepr@gmail.com>
2021-01-05 10:19:00 +0200

    Docs
    Remove module Links from locators.rb

---
+++ links.rb
@@ -0,0 +1,7 @@
+module Links
+  ENTRY_PAGE = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/index.php'
+  NEW_POST = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/post-new.php'
+  NEW_USER = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/user-new.php'
+  USERS = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/users.php'
+  EXPORT = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/export.php'
+end
--- locators.rb
+++ locators.rb
@@ -29,14 +29,6 @@ module UsersLocators
   CHANGE = 'input[id="changeit"]'
 end
 
-module Links
-  ENTRY_PAGE = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/index.php'
-  NEW_POST = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/post-new.php'
-  NEW_USER = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/user-new.php'
-  USERS = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/users.php'
-  EXPORT = 'https://s1.demo.opensourcecms.com/wordpress/wp-admin/export.php'
-end
-
 module ToolsLocators
   STATUS = 'post-status'
 end
